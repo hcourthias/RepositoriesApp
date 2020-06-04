677f309bed51c7c82a4dc7bf5f6459636667ec21
Hugo Courthias <hugo.courthias@gmail.com>
2020-06-04 23:55:38 +0200

    Initial Commit

---
+++ Repositories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Repositories
+//
+//  Created by Hugo Courthias on 04/06/2020.
+//  Copyright © 2020 Hugo Courthias. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
